d9484b194df73d072bedde60a35e7c722988fe87
Aditya Saroha <143502508+Aditya-138-12@users.noreply.github.com>
2025-03-01 18:41:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nodes from './nodes/nodes';
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function App() {
   return (
+    
     <Nodes />
+    
   );
 }
 
